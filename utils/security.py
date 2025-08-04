5deee632f4af660fd5d54ea6a9cd16c4df3def00
OlukaGibson <gibsonoluka7@gmail.com>
2025-08-04 14:17:17 +0300

    initial commit

--- utils/security.py
+++ utils/security.py
@@ -10,10 +10,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
-
 
 def get_current_user(db: Session = Depends(get_db)):
     from models.user_org import User  # only import User here
